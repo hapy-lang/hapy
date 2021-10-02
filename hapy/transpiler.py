b41cabb26e540a9ba78492588f6378ca038e2b26
LeanKhan <eslean20@gmail.com>
2021-10-02 19:52:53 +0100

    add new indent function from Bython

--- hapy/transpiler.py
+++ hapy/transpiler.py
@@ -5,12 +5,36 @@ from input_stream import InputStream
 from token_stream import TokenStream
 from token_parser import parse
 from generate_py import make_py
+from indent import indent
+from exector import run
 
-def transpile(code: str, local: bool = False) -> str:
+def transpile(code: str, local: bool = False, no_indent=False) -> str:
    """transpile .hapy code to python code from string"""
    inputs = InputStream(code)
    tokens = TokenStream(inputs)
    ast = parse(tokens)
-   python_code = make_py(ast, local)
+   unindented_code = make_py(ast, local)
+   # indent python code here...
+   if no_indent:
+      return unindented_code
+
+   python_code = indent(unindented_code)
 
    return python_code
+
+if __name__ == '__main__':
+   code = """
+     if (n < 5) {
+       print("Printed ", n);
+       n = n + 1;
+       print("inside if => ", n);
+       if (n == 1) {
+           print('N is one!')
+       } else {
+           print('N is not one!')
+       }
+   };
+              """
+
+   print(repr(transpile(code, no_indent=False)))
+
