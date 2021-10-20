b9cedf345a1344a8279c387c51152fa6da1bb26c
LeanKhan <eslean20@gmail.com>
2021-10-20 01:58:09 +0100

    return compilation output from exector.run

--- hapy/exector.py
+++ hapy/exector.py
@@ -1,15 +1,10 @@
-"""
-execute python from string! thank you Jesus!
-"""
+"""execute python from string! thank you Jesus!"""
 
 import sys
 import traceback
 import code as _code
 from io import StringIO
-
-codeOut = StringIO()
-codeErr = StringIO()
-
+from importlib import util
 
 # I'm not sure if we need this still. I know sha that we need a way
 # to set the exec know what module is running...
@@ -21,12 +16,30 @@ def run2(source: str, file=False):
     interpreter.runsource(source, '<main>', 'exec')
 
 
-def run(code: str):
+def run(code: str, return_output: bool=False, cloud=False):
+    """Execute Python code
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
+    codeOut = StringIO()
+    codeErr = StringIO()
+
     sys.stdout = codeOut
     sys.stderr = codeErr
 
     try:
-        exec(code)
+        my_spec = util.spec_from_loader("cloud_environment", loader=None)
+        my_module = util.module_from_spec(my_spec)
+
+        if cloud:
+            my_module.__dict__["sys"] = sys
+            # sys.modules[module_name] = my_module
+            exec(code, my_module.__dict__)
+        else:
+            exec(code)
     except Exception as e:
         err = e
         traceback.print_exc()
@@ -36,15 +49,19 @@ def run(code: str):
 
     err = codeErr.getvalue()
 
-    if err:
-        print("error: \n%s\n" % err)
-
     s = codeOut.getvalue()
-
-    print("output: \n%s" % s)
 
     codeOut.close()
     codeErr.close()
+
+    if not return_output:
+        if err:
+            print("error: \n%s\n" % err)
+        print("output: \n%s" % s)
+    else:
+        return err, s
+
+
 
 
 if __name__ == '__main__':
